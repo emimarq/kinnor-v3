165bd26bf9d67aa861f6d8c6a5fa9995a6ad266b
Emilio <emarq@Emilios-MacBook-Air.local>
2026-05-30 02:25:04 -0400

    Initial commit

--- src/views/onboarding.js
+++ src/views/onboarding.js
@@ -3,7 +3,7 @@
 import "../styles/onboarding.css";
 import { renderLessonTree } from "./lessonTree";
 import { renderSignup } from "./signup";
-
+import keyboWaving from "../../public/images/mascot/Keybo-test.svg"
 
 export function renderOnboarding() {
     const app = document.getElementById("app");
@@ -12,30 +12,43 @@ export function renderOnboarding() {
 
     const onboardPrompts = [
         {
-            text: "Learn Music Theory by playing. First, a few questions.",
-            btnTxt: "Let's Go!"
+            text: "Hi, I'm Keybo! I'll be your guide to music theory. Let's answer a few questions.",
+            btnTxt: "Let's Go!",
+            proceed: true,
+            animate: keyboWaving
         },
         {
+            key: "instrument",
             text: "What do you play?",
             options: ["Keys", "Fretted Instrument", "Wind Instrument", "Percussion", "Strings", "None yet"],
             btnTxt: "Continue"
         },
         {
+            key: "goal",
             text: "What's your goal?",
             options: ["Understand music better", "Write my own songs", "Read sheet music", "Just curious"],
             btnTxt: "Continue"
         },
         {
+            key: "knowledge",
             text: "How much do you already know?",
             options: ["I'm brand new", "I know a little", "I know the basics", "I can jam with others", "I write my own music"],
             btnTxt: "Continue"
         },
         {
+            key: "practice",
             text: "How much can you practice daily?",
             options: ["5 minutes", "10 minutes", "30 minutes", "45 minutes", "1 hour"],
             btnTxt: "Continue"
         },
         {
+            key: "age",
+            text: "What is your age?",
+            options: ["6-12", "13-17", "18-24", "25-34", "35-44", "45-54", "55+"],
+            btnTxt: "Continue",
+        },
+        {
+            key: "referral",
             text: "How did you hear about us?",
             options: ["Social media", "Search engine", "A friend", "YouTube", "Other"],
             btnTxt: "Wrap things up!"
@@ -61,19 +74,47 @@ export function renderOnboarding() {
     `
 
     const playArea = document.querySelector("#onboard-play-area");
+    let userResponses = {};
+    let optionSelected = null;
 
+    function updateProgressBar() {
+        const fill = document.querySelector("#progress-bar-fill");
+        const percent = (currentIndex / (onboardPrompts.length)) * 100;
+        fill.style.width = percent + "%";
+    }
 
 
     function render() {
         playArea.innerHTML = `
             <h2>${onboardPrompts[currentIndex].text}</h2>
+            <div id="gif-area-container">
+                <img src="${onboardPrompts[currentIndex].animate}" />
+            </div>
             <div id="choices-area"></div>
             <div class="onboarding-btn-area">
                 <button id="onboarding-cont-btn">${onboardPrompts[currentIndex].btnTxt}</button>
             </div>
         `;
+
+        const gifArea = document.querySelector("#gif-area-container");
+        if (!onboardPrompts[currentIndex].animate) {
+            gifArea.style.display = "none";
+        }
+
+        const continueBtn = document.querySelector("#onboarding-cont-btn");
+
+        if (onboardPrompts[currentIndex].proceed) {
+            continueBtn.disabled = false;
+        } else {
+            continueBtn.disabled = true;
+        }
+
+        continueBtn.addEventListener("click", next);
+
+        optionSelected = null;
         renderOptions();
-        document.querySelector("#onboarding-cont-btn").addEventListener("click", next);
+
+        updateProgressBar();
     }
 
     function renderOptions() {
@@ -87,16 +128,24 @@ export function renderOnboarding() {
                 choicesArea.appendChild(choice);
             })
         }
+
+        gatherResponses();
     }
 
     function next() {
+        //localStorage.setItem("userProfile", JSON.stringify(userResponses));
+
         playArea.classList.remove("onboard-in-right");
         playArea.classList.add("onboard-out-left");
 
         setTimeout(() => {
             currentIndex++;
             if (currentIndex === onboardPrompts.length) {
-                renderSignup();
+                updateProgressBar();
+                setTimeout(() => {
+                    renderSignup();
+                }, 550)
+                return;
             }
             playArea.classList.remove("onboard-out-left");
             render();
@@ -105,4 +154,24 @@ export function renderOnboarding() {
     }
 
     render();
+
+    function gatherResponses() {
+        const choices = document.querySelectorAll(".choices");
+
+        choices.forEach((choice) => {
+            const continueBtn = document.querySelector("#onboarding-cont-btn");
+
+            choice.addEventListener("click", (e) => {
+                if (optionSelected) {
+                    optionSelected.classList.remove("option-selected");
+                }
+                optionSelected = e.target;
+                optionSelected.classList.add("option-selected");
+                continueBtn.disabled = false;
+
+                userResponses[onboardPrompts[currentIndex].key] = e.target.textContent;
+                console.log(userResponses);
+            })
+        })
+    }
 }
