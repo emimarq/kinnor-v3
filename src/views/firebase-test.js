b6ed0d39de458cd5f92d355870dcb3fb94c989ad
Emilio <emarq@Emilios-MacBook-Air.local>
2026-05-31 00:53:44 -0400

    successful sign-up test

--- src/views/firebase-test.js
+++ src/views/firebase-test.js
@@ -8,4 +8,12 @@ export function renderTesting() {
         <input name="password" type="password" placeholder="password"/>
         <button id="submit-btn">Submit</button>
     `
+    const email = document.querySelector('[name="email"]');
+    const password = document.querySelector('[name="password"]');
+
+    const submitBtn = document.getElementById("submit-btn");
+    submitBtn.addEventListener("click", async () => {
+        const idkName = await signUp(email.value, password.value);
+        console.log(idkName);
+    })
 }
